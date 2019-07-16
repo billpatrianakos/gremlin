35d972df675877e06b10e110de505875057550d0
Bill Patrianakos <bill@billpatrianakos.me>
2019-07-16 16:31:00 -0500

    Beginning work on routes.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,47 @@
 // Main server setup and startup script
 
-const express = require('express');
-const app     = express();
-const path    = require('path');
+const express     = require('express');
+const app         = express();
+const path        = require('path');
+const _           = require('lodash');
+const morgan      = require('morgan');
+const session     = require('express-session');
+const bodyParser  = require('body-parser');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-if (process.env.NODE_ENV === 'production') app.use(express.static('public'));
+if (_.includes(['development', 'test'], process.env.NODE_ENV)) {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(morgan('combined'));
+} else {
+  app.set('trust proxy', 1);
+  app.use(morgan('combined', {
+    skip: (req, res) => { return res.statusCode < 400 },
+    stream: fs.createWriteStream(__dirname + '/log/application.log', { flags: 'a' })
+  }));
+}
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  name: 'gsynth.sid',
+  resave: false,
+  rolling: true,
+  saveUninitialized: false,
+  secret: 'synth5ar3k3wl!', // You should change this
+  unset: 'destroy'
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use('csurf'); // TODO: Implement CSURF
+
+
+require('./routes')(app);
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
 
 let server = app.listen(process.env.SERVER_PORT || 9000, () => {
   console.log(`Gremlin server listening on localhost:${server.address().port} in ${process.env.NODE_ENV} mode`);
---
+++ server/routes/index.js
@@ -0,0 +1,23 @@
+// Route setup
+// ===========
+
+'use strict';
+
+let _         = require('lodash'),
+    fs        = require('fs'),
+    path      = require('path'),
+    excluded  = ['index', 'default'];
+
+module.exports = function(app) {
+  fs.readdirSync(__dirname).forEach((file) => {
+    // Remove extension from file name
+    var basename = file.split('.')[0];
+
+    // Do not load directories or blacklisted file names
+    if (!fs.lstatSync(__dirname + '/' + file).isDirectory() && !_.includes(excluded, file)) {
+      app.use('/' + basename, require('./' + file));
+    }
+
+    if (file === 'default') app.use('/', require('./default'));
+  });
+};
