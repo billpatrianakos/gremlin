0faf9c46476e4eff000f36ba8690a99e41075f79
Bill Patrianakos <bill@billpatrianakos.me>
2019-07-28 19:38:23 -0500

    Beginning auth process. Code is there but there is an issue in the functionality.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,13 +4,38 @@
 'use strict';
 
 const express        = require('express'),
-      AuthController = express.Router();
+      AuthController = express.Router(),
+      User           = require(__dirname + '/../models/user'),
+      bcrypt         = require('bcrypt');
 
 AuthController.route('/?')
   // GET /
   // -----
   .get((req, res, next) => {
     res.render('auth', { messages: req.flash('info') });
+  });
+
+AuthController.route('/login/?')
+  .post((req, res, next) => {
+    { username, password } = req.body;
+
+    new User({ username: username })
+      .fetch(user => {
+        bcrypt.compare(password, user.get('password'))
+          .then(response => {
+            if (response) {
+              req.session.userId = user.get('id');
+              req.session.loggedIn = true;
+              res.redirect('/');
+            } else {
+              res.status(401).render('auth', { messages: ['Username or password was incorrect'] });
+            }
+          })
+      })
+      .catch(err => {
+        req.flash('error', err);
+        res.status(401).redirect;('/auth');
+      });
   });
 
 // AuthController.route('/login/?')
